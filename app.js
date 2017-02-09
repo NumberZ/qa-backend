1601e406806d581ca98d478c07d535235baf1353
Vinnie <215903432@qq.com>
2017-02-10 01:08:52 +0800

    first

---
+++ app.js
@@ -0,0 +1,31 @@
+const koa = require('koa');
+const app = koa();
+const Router = require('koa-router');
+
+const sign = require('./sign.js');
+
+const router = new Router({
+    prefix: '/api'
+});
+
+// logger
+app.use(function *(next){
+  const start = new Date;
+  yield next;
+  const ms = new Date - start;
+  console.log('%s %s - %s', this.method, this.url, ms);
+});
+
+
+router.get('/sign', function *(next) {
+  const res = sign(this.query.jsApiTicket, this.query.localUrl);
+  this.body = res;
+  yield next;
+});
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+app.listen(3000, () => {
+    console.log('app is listing on 3000');
+});
