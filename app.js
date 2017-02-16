d3412a341c2c49fc60fff8f6ee86d674b961359d
Vinnie <215903432@qq.com>
2017-02-16 14:58:06 +0800

    complete uploadVioce to qiniu

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const koa = require('koa');
 const app = koa();
 const Router = require('koa-router');
-
+const fs = require('fs');
+const http = require('http');
+const weixin = require('weixin-node');
+const request = require('request');
 const sign = require('./sign.js');
+const uploadVoice = require('./uploadVoice');
 
 const router = new Router({
     prefix: '/api'
 });
+
+const wxConfig = {
+ appId: 'wx039613f59e8ce6c4',
+ secret: '8e5149e62d86e15f51080a4cd88e575b'
+}
+
+
 
 // logger
 app.use(function *(next){
@@ -22,13 +33,33 @@ app.use(function *(next) {
 });
 
 router.get('/sign', function *(next) {
-  const res = sign(this.query.jsApiTicket, this.query.localUrl);
+  const token = yield weixin.getToken(wxConfig.appId, wxConfig.secret);
+  console.log('token:', token);
+  const ticket = yield weixin.getTicket(token.access_token);
+  console.log('ticket:', ticket)
+  const res = sign(ticket.ticket, this.query.localUrl);
+  console.log('res:', res);
   this.body = res;
   yield next;
 });
 
-
-
+router.get('/uploadVoice', function *(next) {
+  const serverId = this.query.serverId;
+  const token = yield weixin.getToken(wxConfig.appId, wxConfig.secret);
+  console.log(`http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=${token.access_token}&media_id=${serverId}`);
+  const url = `http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=${token.access_token}&media_id=${serverId}`;
+  const time = String(new Date().getTime());
+  request(url)
+  .on('response', (response) => {
+    response.on('end', () => {
+      console.log('end');
+      uploadVoice(`${time}.amr`);
+    })
+  })
+  .pipe(fs.createWriteStream(`${time}.amr`))
+  // uploadVoice(res);
+  this.body = `http://file.api.weixin.qq.com/cgi-bin/media/get?access_token=${token.access_token}&media_id=${serverId}`;
+})
 app
   .use(router.routes())
   .use(router.allowedMethods());
---
+++ uploadVoice.js
@@ -0,0 +1,43 @@
+const qiniu = require('qiniu');
+const qnConfig = {
+  access_key: 'Dwzp6cGfVhaZ70YLOBtKXt6orlKw2kAgHuH2AGdW',
+  secret_key: 'xnmXns5ac9asQ1ZqoxzypJvGxCNlcFwGwLif0UZV'
+}
+
+qiniu.conf.ACCESS_KEY = qnConfig.access_key;
+qiniu.conf.SECRET_KEY = qnConfig.secret_key;
+
+const bucket = 'vinnie';
+
+
+const pipleline = 'anwser';
+const fops = "avthumb/mp3";
+
+function uptoken(bucket, key) {
+  const putPolicy = new qiniu.rs.PutPolicy(bucket);
+  putPolicy.persistentOps = fops;
+  putPolicy.persistentPipeline = pipleline;
+  return putPolicy.token();
+}
+
+
+
+function uploadBuff(body, key, uptoken) {
+  const extra = new qiniu.io.PutExtra();
+  qiniu.io.putFile(uptoken, key, body, extra, (err, ret) => {
+    if (!err) {
+      console.log(ret);
+    } else {
+      console.log(err);
+    }
+  })
+}
+
+
+function uploadFile(body) {
+  const key = String(new Date().getTime()) + '.mp3';
+  const token = uptoken(bucket, key);
+  return uploadBuff(body, key, token);
+}
+
+module.exports = uploadFile;
